3e34ec6bcd7bc9249ccff462b29244bbd66aa5d1
Gabriel Carvalho <gabrielcarvalhogrd@gmail.com>
2024-08-28 22:13:30 -0300

    ..

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -1,16 +1,31 @@
 const md5 = require('md5')
+
+const pool = require('../config/banco-de-dados')
+
 const usuarios = []
 
 function adicionarUsuario(nome, email, senha){
-    usuarios.push({
-        id: Date.now(),
-        nome: nome,
-        email: email,
-        senha: md5(senha),
-        criadoEm: new Date()
+    // usuarios.push({
+    //id: Date.now(),
+    //  nome: nome,
+    // email: email,
+    //senha: md5(senha),
+    //  criadoEm: new Date()
+    // })
+
+    // console.log(usuarios)
+
+    const senhaCriptografada = md5(senha)
+
+    // Adicionando um novo usuario no banco de dados
+    pool.query(`INSERT INTO usuarios(Nome, email, senha, criadoEm) VALUES ('${nome}', '${email}', '${senhaCriptografada}', NOW())`)
+    .then(() => {
+        console.log('DEU CERTO')
+    })
+    .catch(error => {
+        console.error('ERRO AO INSERIR DADOS', error)
     })
 
-    console.log(usuarios)
 }
 
 module.exports = {
